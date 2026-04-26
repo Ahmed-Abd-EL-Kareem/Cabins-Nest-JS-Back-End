5fc9434922130d0a2e401d238a85b7d86270f1a2
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2026-04-26 19:27:00 +0300

    Production-2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './common/public';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Public()
+  @Get('health')
+  healthCheck(): { status: string } {
+    return { status: 'ok' };
   }
 }
