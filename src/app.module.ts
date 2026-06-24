e91b44f4cb3ca0ce15a77841ac07378d62c84679
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2026-06-24 22:32:01 +0300

    Fix GraphQL schema generation on Vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,7 +51,7 @@ import { RolesGuard } from './common/guards/role.guard';
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       playground: false,
       introspection: true,
-      autoSchemaFile: join(process.cwd(), './schema.gql'),
+      autoSchemaFile: join(process.cwd(), 'schema.gql'),
       formatError: (formattedError: GraphQLFormattedError, error: unknown) => {
         // 1. Create a safe interface for the NestJS error structure
         interface NestJsError {
